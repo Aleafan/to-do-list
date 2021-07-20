102b1ec93f30d7d6d295908783e1a953019d932a
Aleafan <ae.afanasov@yandex.com>
2021-07-20 09:55:22 +0300

    Import and use functions loadInterface, createHeader, createNavMenu, createProjectPage. Add description props to projects, add props notes, dueDate, complete for tasks

--- src/index.js
+++ src/index.js
@@ -1,99 +1,120 @@
-import { loadContent } from './domChange.js';
-import { createMenu } from './mainMenu.js';
+import { loadInterface, loadContent } from './domChange.js';
+import { createHeader } from './header.js';
+import { createNavMenu } from './navMenu.js';
+import { createProjectPage } from './project.js';
 
 const vacation = {
   title: 'Vacation in Rome',
+  description: 'We\'ll go from June 14-22 and visit Rome, Siena and Florence', 
   tasks: [
     {
       title: 'Book flight',
-      description: '',
-      dueDate: '',
+      notes: '',
+      dueDate: 'Sep 28',
       priority: 1,
+      complete: 1,
     },
     {
       title: 'Read about the metro',
-      description: '',
-      dueDate: '',
+      notes: '',
+      dueDate: 'Today',
       priority: 0,
+      complete: 0,
     },
     {
       title: 'Buy travel guide',
-      description: '',
-      dueDate: '',
+      notes: '',
+      dueDate: 'Sep 12',
       priority: 0,
+      complete: 1,
     },
     {
       title: 'Book hotel room',
-      description: '',
+      notes: '',
       dueDate: '',
-      priority: 0,
+      priority: 1,
+      complete: 0,
     }
   ]
 }
 const exercise = {
   title: 'Exercise',
+  description: 'Phisical exercise is important for general well-being', 
   tasks: [
     {
       title: 'Practice yoga',
-      description: 'Practice yoga 3 times per week',
+      notes: 'Practice yoga 3 times per week',
       dueDate: '',
       priority: 1,
+      complete: 0,
     },
     {
       title: 'Exercise on the bar',
-      description: '',
+      notes: '',
       dueDate: '',
       priority: 0,
+      complete: 0,
     },
     {
       title: 'Do back exercises',
-      description: '',
+      notes: '',
       dueDate: '',
       priority: 0,
+      complete: 0,
     }
   ]
 }
 const health = {
   title: 'Health',
+  description: 'Caring for my health leads to better quality of life', 
   tasks: [
     {
       title: 'Have healthy diet',
-      description: '',
+      notes: '',
       dueDate: '',
       priority: 0,
+      complete: 1,
     },
     {
       title: 'Reduce sugar consumption',
-      description: '',
+      notes: '',
       dueDate: '',
       priority: 0,
+      complete: 0,
     },
     {
       title: 'Drink less alcohole',
-      description: '',
+      notes: '',
       dueDate: '',
       priority: 0,
+      complete: 1,
     }
   ]
 }
 const house = {
   title: 'House',
+  description: 'Clean the house once every week', 
   tasks: [
     {
       title: 'Clean the house',
-      description: 'Once per week',
+      notes: 'Once per week',
       dueDate: '',
       priority: 0,
+      complete: 1,
     },
     {
       title: 'Repair broken tap',
-      description: '',
+      notes: '',
       dueDate: '',
       priority: 0,
+      complete: 0,
     }
   ]
 }
 
 export const projects = [vacation, exercise, health, house];
 
-loadContent(createMenu());
+loadInterface(createHeader());
+loadInterface(createNavMenu());
+
+loadContent(createProjectPage(projects[0]));

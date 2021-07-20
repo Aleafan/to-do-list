31b43b33306bbcb287e008913c0881ea356142d7
Aleafan <ae.afanasov@yandex.com>
2021-07-20 09:52:34 +0300

    Add loadInterface function for loading app's header and nav bar. Correct loadContent function

--- src/domChange.js
+++ src/domChange.js
@@ -1,9 +1,14 @@
+function loadInterface(element) {
+  document.body.appendChild(element);
+}
+
 function loadContent(content) {
-  const previousContent = document.getElementById('content');
+  const previousContent = document.querySelector('main');
   if (previousContent) {
     document.body.removeChild(previousContent);
   }
   document.body.appendChild(content);
+  document.body.classList.remove('nav-positioner');
 }
 
-export { loadContent };
+export { loadInterface, loadContent };

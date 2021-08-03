3de9dbc6d83f349967aa04249018dea9c629226d
Aleafan <ae.afanasov@yandex.com>
2021-08-03 14:47:05 +0300

    Add functions createElemAttr, formatDate

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,11 @@
+import { parse, format, isToday, isTomorrow, isThisYear, isPast } from 'date-fns';
+
+
 // Unique ID generator (based on https://gist.github.com/gordonbrander/2230317#file-id-js)
 function uniqueId() {
   return 'a' + Math.random().toString(36).substr(2, 9);
 }
+
 
 function setAttributes(element, attributes) {
   for (const key in attributes) {
@@ -9,4 +13,25 @@ function setAttributes(element, attributes) {
   }
 }
 
-export { uniqueId, setAttributes };
+
+function createElemAttr(element, attributes) {
+  const newElement = document.createElement(element);
+  for (const key in attributes) {
+    newElement.setAttribute(key, attributes[key]);
+  }
+  return newElement;
+}
+
+
+function formatDate(dateStr) {
+  const parsedDate = parse(dateStr, "MMM d yyyy", new Date());
+  const expired = isPast(parsedDate);
+  const date = isToday(parsedDate) ? 'Today'
+      : isTomorrow(parsedDate) ? 'Tomor.'
+      : isThisYear(parsedDate) ? format(parsedDate, 'MMM d')
+      : dateStr;
+  return { date, expired };
+}
+
+
+export { uniqueId, setAttributes, createElemAttr, formatDate };

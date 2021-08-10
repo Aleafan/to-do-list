86c84af5e4c00340eac39f2917a0c56bfe96fa9a
Aleafan <ae.afanasov@yandex.com>
2021-08-10 11:45:02 +0300

    Add to createNavMenu form creation functionality, refactor code using createElemAttr function, add toggleProjectForm(), handleSubmit(), createProjectButton

--- src/navMenu.js
+++ src/navMenu.js
@@ -1,84 +1,127 @@
-import { projects } from './data.js';
+import { projects, createProject } from './data.js';
 import { createProjectPage } from './project.js';
 import { loadContent } from './domChange.js';
-import { setAttributes } from './helpers.js';
+import { createElemAttr } from './helpers.js';
 
 const timeline = ['Today', 'This week', 'All tasks'];
 
 function createNavMenu() {
-  const navWrapper = document.createElement('div');
-  navWrapper.setAttribute('id', 'nav-wrapper');
+  const navWrapper = createElemAttr('div', {id: 'nav-wrapper'});
 
-  const navMenu = document.createElement('section');
-  navMenu.setAttribute('id', 'nav-menu');
+  const navMenu = createElemAttr('section', {id: 'nav-menu'});
   navWrapper.appendChild(navMenu);
 
-  const navTimeline = document.createElement('ul');
-  navTimeline.setAttribute('id', 'nav-timeline');
+  const navTimeline = createElemAttr('ul', {id: 'nav-timeline'});
   navMenu.appendChild(navTimeline);
 
   timeline.forEach(time => {
     const li = document.createElement('li');
     navTimeline.appendChild(li);
 
-    const button = document.createElement('button');
-    setAttributes(button, {'class': 'btn-menu btn-flex', 'type': 'button'});
+    const button = createElemAttr('button', {class: 'btn-menu btn-flex', type: 'button'});
     li.appendChild(button);
 
     const span1 = document.createElement('span');
     span1.textContent = time;
-    button.appendChild(span1);
 
     const span2 = document.createElement('span');
     span2.textContent = '5';
-    button.appendChild(span2);
+
+    button.append(span1, span2);
   });
 
-  const navProjects = document.createElement('ul');
-  navProjects.setAttribute('id', 'nav-projects');
+  const navProjects = createElemAttr('ul', {id: 'nav-projects'});
   navMenu.appendChild(navProjects);
 
   const h2 = document.createElement('h2');
   h2.textContent = 'Projects';
   navProjects.appendChild(h2);
 
-  projects.forEach(project => {
-    const li = document.createElement('li');
-    li.setAttribute('id', project.id);
-    navProjects.appendChild(li);
+  projects.list.forEach(project => createProjectButton(project, navProjects));
 
-    const button = document.createElement('button');
-    setAttributes(button, {'class': 'btn-menu btn-flex', 'type': 'button'});
-    li.appendChild(button);
+  // Create form for adding new project
+  const projectFormWrapper = createElemAttr('div', {id: 'project-form-wrapper'});
+  navMenu.appendChild(projectFormWrapper);
 
-    const spanTitle = document.createElement('span');
-    spanTitle.textContent = project.title;
-    button.appendChild(spanTitle);
+  const form = createElemAttr('form', {id: 'project-form'});
+  projectFormWrapper.appendChild(form);
 
-    const spanNumber = document.createElement('span');
-    spanNumber.classList.add('task-number')
-    spanNumber.textContent = project.tasks.length;
-    button.appendChild(spanNumber);
-    
-    button.addEventListener('click', () => loadContent(createProjectPage(project)))
-  });
+  const labelTitle = createElemAttr('label', {for: 'project-title'});
+  labelTitle.appendChild(createElemAttr('i', {class: 'fas fa-square project-icon'}));
+  const labelTitleText = createElemAttr('span', {class: 'visuallyhidden'});
+  labelTitleText.textContent = 'Project name';
+  labelTitle.appendChild(labelTitleText);
+
+  form.appendChild(labelTitle);
+
+  const inputTitle = createElemAttr('input', {type: 'text', class: 'project-title', placeholder: 'Project name', 
+      required: ''});
+  form.appendChild(inputTitle);
+
+  const btnSubmit = createElemAttr('button', {type: 'submit', class: 'task-option', 'aria-label': 'add-new-project'});
+  btnSubmit.appendChild(createElemAttr('i', {class: 'fas fa-plus'}));
+  form.appendChild(btnSubmit);
   
-  const btnAdd = document.createElement('button');
-  setAttributes(btnAdd, {'class': 'btn-menu btn-add', 'type': 'button'});
+  const btnAdd = createElemAttr('button', {class: 'btn-menu btn-add', type: 'button'});
   btnAdd.textContent = ' Add project';
-  navMenu.appendChild(btnAdd);
+  projectFormWrapper.appendChild(btnAdd);
+
+  // Add event listeners
+  btnAdd.addEventListener('click', toggleProjectForm);
+
+  form.addEventListener('submit', handleSubmit);
 
   return navWrapper;
+}
+
+function createProjectButton(project, domParent) {
+  const li = createElemAttr('li', {id: project.id});
+  domParent.appendChild(li);
+
+  const button = createElemAttr('button', {class: 'btn-menu btn-flex', type: 'button'});
+  button.appendChild(createElemAttr('i', {class: 'fas fa-square project-icon'}));
+  li.appendChild(button);
+
+  const spanTitle = document.createElement('span');
+  spanTitle.textContent = project.title;
+  button.appendChild(spanTitle);
+
+  const spanNumber = createElemAttr('span', {class: 'task-number'});
+  spanNumber.textContent = project.tasks.length;
+  button.appendChild(spanNumber);
+  
+  button.addEventListener('click', () => loadContent(createProjectPage(project)));
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const title = this.querySelector('.project-title').value;
+  const newProject = createProject(title);
+  projects.addProject(newProject);
+  loadContent(createProjectPage(newProject));
+  toggleProjectForm();
+  createProjectButton(newProject, document.getElementById('nav-projects'));
 }
 
 function toggleNav() {
   document.body.classList.toggle('nav-positioner');
 }
 
+function toggleProjectForm() {
+  const projectFormWrapper = document.getElementById('project-form-wrapper');
+  if (projectFormWrapper.classList.contains('show-form')) {
+    projectFormWrapper.classList.remove('show-form');
+    projectFormWrapper.querySelector('form').reset();
+  } else {
+    projectFormWrapper.classList.add('show-form');
+    projectFormWrapper.querySelector('.project-title').focus();
+  }
+}
+
 function recalcTaskNumber(project) {
   const navProjects = document.getElementById('nav-projects');
   const spanNumber = navProjects.querySelector(`#${project.id} .task-number`);
-  spanNumber.textContent = project.tasks.length;
+  spanNumber.textContent = project.calcTasks();
 }
 
 export { createNavMenu, toggleNav, recalcTaskNumber };

e529e358ccc6af2fa1941e9c845dac28f97116b8
Aleafan <ae.afanasov@yandex.com>
2021-08-26 18:55:09 +0300

    Add execution of prepareUsersData() and configureDataSave() before first load

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import { prepareUsersData, configureDataSave } from './data';
 import { loadInterface, loadContent } from './domChange';
 import { createHeader } from './header';
 import { createNavMenu } from './navMenu';
 import { createTodayPage } from './today';
+
+prepareUsersData();
+configureDataSave();
 
 loadInterface(createHeader());
 loadInterface(createNavMenu());

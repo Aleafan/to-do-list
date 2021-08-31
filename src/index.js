acb522296eb19e5b99294b130bc3b89259318280
Aleafan <ae.afanasov@yandex.com>
2021-08-31 10:12:54 +0300

    Import css files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { prepareUsersData, configureDataSave } from './data';
 import { loadContent } from './domFunctions';
 import { createNavMenu } from './navMenu';
 import createTodayPage from './today';
+import './styles/normalize.css';
+import './styles/style.css';
+import './styles/dark.css';
 
 prepareUsersData();
 configureDataSave();
